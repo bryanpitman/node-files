471c70621f9ef2e384076fdcd408d2e482ff6b2c
Bryan Pitman <pitman.bryan@gmail.com>
2022-08-16 11:59:37 -0700

    part 1 complete

---
+++ step1.js
@@ -0,0 +1,13 @@
+const fsP = require("fs/promises");
+
+async function cat(path) {
+  try {
+    let contents = await fsP.readFile(path, "utf8");
+    console.log("file contents", contents);
+  } catch (err) {
+    console.log(err)
+    process.exit(1);
+  }
+}
+console.log(process.argv)
+cat(process.argv[2])
